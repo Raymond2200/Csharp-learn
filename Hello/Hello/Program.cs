ea99af0c4d9bac4feaf2fd985039d24085b76248
raymond <raycasgrain@gmail.com>
2021-06-09 11:44:05 -0400

    first commit

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int scores = 0;
+            string command = "";
+            int score;
+            bool scoreConvert;
+
+            while (command != "-1")
+            {
+                Console.WriteLine("enter student score");
+                command = Console.ReadLine();
+                scoreConvert = int.TryParse(command, out score);
+                if (scoreConvert)
+                {
+                    if (score == -1)
+                    {
+                        Console.WriteLine(scores / counter);
+                        break;
+                    }
+                    else if (score >= 0 && score <= 20)
+                    {
+                        scores += score;
+                        counter += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("invalid entry, must be between 0 and 20 or -1");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("you must enter a number");
+                }
+            }
+        }
+    }
+}
