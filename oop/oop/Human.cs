3b529394922438ca4b6f271d32220cbd357585c5
raymond <raycasgrain@gmail.com>
2021-06-09 11:58:43 -0400

    classes

---
+++ oop/oop/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    // this class is a blueprint for datatype
+    class Human
+    {
+
+        // member variable
+        public string firstName;
+        public string lastName;
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi I'm " + firstName + " "+ lastName);
+        }
+    }
+}
---
+++ oop/oop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is how an object of a class is created
+            Human denis = new Human();
+
+            denis.firstName = "Denis";
+
+            denis.IntroduceMyself();
+
+            Human michael = new Human();
+
+            michael.firstName = "Michael";
+
+            michael.IntroduceMyself();
+        }
+    }
+}
